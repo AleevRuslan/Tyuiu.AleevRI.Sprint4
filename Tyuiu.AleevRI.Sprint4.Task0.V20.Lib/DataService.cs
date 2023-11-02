5431d90ec46582a414a3e9743a70ec4428ce3a55
Tyuiu.AleevRI@outlook.com <aleevruslan3@gmail.com>
2023-11-03 01:53:28 +0500

    Add project files.

---
+++ Tyuiu.AleevRI.Sprint4.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AleevRI.Sprint4.Task0.V20.Lib
+{
+    public class DataService : ISprint4Task0V20
+    {
+        public int GetMultEvenArrEl(int[] array)
+        {
+            int num = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    num *= array[i];
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ Tyuiu.AleevRI.Sprint4.Task0.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AleevRI.Sprint4.Task0.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AleevRI.Sprint4.Task0.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e363fc1-3a7a-4af6-aca2-1ab863e085ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
